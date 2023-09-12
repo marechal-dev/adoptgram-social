2ec06daa89105c49f4d106965a0f953feaf494b2
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-09-12 11:38:57 -0300

    fix: update e2e tests setup

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,18 +1,16 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
-
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
+    root: './',
     globals: true,
-    environmentMatchGlobs: [['src/infra/http/controllers/**', 'prisma']],
+    include: ['**/*.e2e-spec.ts'],
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
-    //@ts-ignore
     tsconfigPaths(),
-    //@ts-ignore
     swc.vite({
       module: { type: 'es6' },
     }),
