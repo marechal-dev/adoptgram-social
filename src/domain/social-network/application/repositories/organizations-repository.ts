bffebb7d28e5e65403426752f79f6003f05f92dc
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-11-13 00:04:01 -0300

    fix: delete unused file, update typedef

--- src/domain/social-network/application/repositories/organizations-repository.ts
+++ src/domain/social-network/application/repositories/organizations-repository.ts
@@ -1,12 +1,7 @@
-import { PaginationParams } from '@Core/repositories/pagination-params';
 import { Organization } from '@Domain/social-network/enterprise/entities/organization';
 
 export abstract class OrganizationsRepository {
   abstract create(organization: Organization): Promise<void>;
-  abstract findManyByTitle(
-    title: string,
-    paginationParams: PaginationParams,
-  ): Promise<Organization[]>;
   abstract findById(id: string): Promise<Organization | null>;
   abstract findByUsername(username: string): Promise<Organization | null>;
   abstract findByEmail(email: string): Promise<Organization | null>;
